dbf63a1db17c476d50686f11726b0198ee306610
oliver alexander <rodriguezarrocha@gmail.com>
2017-02-04 16:13:58 -0500

    filtering

---
+++ as10_filtering/js/footer.js
@@ -0,0 +1,5 @@
+var msg = '<p>' + document.title + '<br />';
+msg += 'source: DC Comics<br />';
+msg += '<b>Oliver Alexander </b><br />';
+var el = document.getElementById('footer');
+el.innerHTML = msg;
